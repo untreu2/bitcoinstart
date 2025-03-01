27e9f87d371e2535c841c2d6ffdaeaa24e7fd0ba
efecini <efecini@gmail.com>
2025-03-02 01:41:13 +0300

    make the questions continuous

--- src/main.rs
+++ src/main.rs
@@ -11,26 +11,31 @@ fn main() {
     println!("2 - generate xpub & zpub from mnemonic phrase");
     println!("3 - generate bech32 addresses from xpub or zpub");
 
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Error...");
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Error...");
+        match input.trim() {
+            "1" => {
+                let output = options::genwords::main();
+                println!("{:?}", output);
+                break;
+            }
 
-    match input.trim() {
-        "1" => {
-            let output = options::genwords::main();
-            println!("{:?}", output);
-        }
+            "2" => {
+                let output = options::genpub::main();
+                println!("{:?}", output);
+                break;
+            }
 
-        "2" => {
-            let output = options::genpub::main();
-            println!("{:?}", output);
-        }
-
-        "3" => {
-            let output = options::genaddress::main();
-            println!("{:?}", output);
-        }
-        _ => {
-            println!("Invalid input.");
+            "3" => {
+                let output = options::genaddress::main();
+                println!("{:?}", output);
+                break;
+            }
+            _ => {
+                println!("Invalid input.");
+                continue;
+            }
         }
     }
 }
